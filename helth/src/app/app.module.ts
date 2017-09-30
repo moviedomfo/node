ede27d40c7ec70eb1714e5799faf47ebef93e4b8
Marcelo Oviedo <marcelo.oviedo@gmail.com>
2017-09-29 22:30:33 -0300

    d

--- helth/src/app/app.module.ts
+++ helth/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule ,ReactiveFormsModule} from '@angular/forms';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AlertModule } from 'ngx-bootstrap';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
@@ -12,8 +12,8 @@ import { AppComponent } from './app.component';
 
 
 import { BootstrapModalModule } from 'ng2-bootstrap-modal';
-import {CommonService,PatientsService,PersonsService} from './service/index';
-import { rutesModule }        from './app.routing';
+import {CommonService, PatientsService, PersonsService} from './service/index';
+import { rutesModule } from './app.routing';
 import {AuthGuard} from './commonComponents/routingGuard/AuthGuard';
 import { PersonsComponent } from './persons/persons.component' ;
 import { PatientComponent } from './patient/patient.component';
@@ -27,7 +27,7 @@ import { PatientGridComponent } from './patient/patient-grid/patient-grid.compon
 import {DateComponent} from "./commonComponents/ag-grid/date.component";
 import {HeaderComponent} from "./commonComponents/ag-grid/header.component";
 import {HeaderGroupComponent} from "./commonComponents/ag-grid/header-group.component";
-//commonComponents
+// commonComponents
 import { PageNotFoundComponent } from './commonComponents/page-not-found/page-not-found.component';
 import { ModalDialogComponent } from './commonComponents/modal-dialog/modal-dialog.component';
 import { FontAgComponent } from './commonComponents/font-ag/font-ag.component';
@@ -71,7 +71,7 @@ import {
   MdStepperModule,
 } from '@angular/material';
 import { PatientMangerComponent } from './patient/patient-manger/patient-manger.component';
-import { PersonAddressComponent } from './person/persons/person-address/person-address.component';
+import { PersonAddressComponent } from './persons/person-address/person-address.component';
 
 @NgModule({
   declarations: [
