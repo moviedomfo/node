57f265cb3b7996136c2c0a4e92fbb6706819de3e
Marcelo Oviedo <marcelo.oviedo@gmail.com>
2017-12-31 17:02:06 -0300

    delete old health

--- helth/src/app/model/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './common.constants';
-export * from './common.model';
-export * from './patients.model';
-export * from './persons.model';
--- helth/src/app/service/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './common.service';
-export * from './patients.service';
-export * from './persons.service';
-export * from './authentication.service';
-export * from './medical-Insurance.service';
