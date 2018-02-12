55e963417290c22f4552d4ac86ae600b01d6a253
Marcelo F Oviedo <marcelo.oviedo@gmail.com>
2018-02-12 13:25:41 -0300

    svc q consume server remoto con autenticacion

--- testSecurity/src/app/service/posts-and-comments.service.ts
+++ testSecurity/src/app/service/posts-and-comments.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { catchError } from 'rxjs/operators';
 
@@ -7,18 +7,39 @@ import { catchError } from 'rxjs/operators';
 @Injectable()
 export class PostsAndCommentsService {
 // Define the routes we are going to interact with
-private publicDealsUrl = 'http://localhost:8080/api/deals/public';
-private privateDealsUrl = 'http://localhost:8080/api/deals/private';
+private publicDealsUrl = 'http://localhost:8080/postList';
+private privateDealsUrl = 'http://localhost:8080/commentList';
 
   constructor(private http: HttpClient) { }
 
 
-  retryveAllPostService(){
+  retriveAllPostService(){
+    return this.http
+      .get(this.publicDealsUrl)
+      .pipe(
+        catchError(this.handleError)
+      );
 
+  
   }
 
 
-  retryveAllCommentsService(){
-    
+  retriveAllCommentsService(){
+    return this.http
+    .get(this.privateDealsUrl, {
+      headers: new HttpHeaders().set('Authorization', `Bearer ${localStorage.getItem('token')}`)
+    })
+    .pipe(
+      catchError(this.handleError)
+    );
+  }
+  // Implement a method to handle errors if any
+  private handleError(err: HttpErrorResponse | any) {
+    console.error('An error occurred', err);
+    return Observable.throw(err.message || err);
+  }
+
+  purchase(item) {
+    alert(`You bought the: ${item.name}`);
   }
 }
